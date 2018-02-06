6a6f123d20cef8f732dcd37999f85b958bb38214
Rylee Andrews <ryleeandrews73@gmail.com>
2018-02-05 20:30:07 -0800

    hopefully fixes merge issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 require('dotenv').config();
-console.log(process.env.CORS_ORIGINS);
+console.log(process.env.PORT);
 const PORT = process.env.PORT || 5000;
 const DB = process.env.DB_URL || 'mongodb://localhost:27017/visual_files_dev';
 
